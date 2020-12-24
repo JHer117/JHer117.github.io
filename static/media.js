0c9a205b73f66d210b9c9d3596a8da6fffe0b067
Snacks <whiteintheiris@gmail.com>
2020-12-24 01:19:28 -0800

    first commit changeover to SPA site

---
+++ static/media.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+
+    document.querySelectorAll('a[href^="#up_top"]').forEach($anchor => {
+        $anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    });
+
+    document.querySelectorAll('a[href^="#packages_link"]').forEach($anchor => {
+        $anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    });
+
+
+    document.querySelectorAll('a[href^="#about_section"]').forEach($anchor => {
+        $anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    });
+
+
+    document.querySelectorAll('a[href^="#contact"]').forEach($anchor => {
+        $anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    });
+
+
+
+
+
+
+
+
+
+
+});
